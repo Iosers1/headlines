c2a89fd08cd41e5267ad3f1cd0ed08bfdb2516b2
Mr.Hou <1492642409@qq.com>
2022-09-05 16:52:25 +0800

    导入了字体图标库

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+/* eslint-disable comma-dangle */
+/* eslint-disable semi */
+/* eslint-disable quotes */
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 // 全局样式文件
-import '@/styles/index.less'
+import "@/styles/index.less";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
